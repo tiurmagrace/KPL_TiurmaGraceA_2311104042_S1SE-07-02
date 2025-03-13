d44f3803c409700a0e669e08284e7b7a643b970c
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-14 04:26:40 +0700

    menambahkan implementasi dengan state-based

---
+++ 04_Automata_dan_Table-Driven_Constructionn/tpmod4_2311104042/tpmod4_2311104042/DoorMachine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class DoorMachine
+{
+    // Enum untuk mendefinisikan state pintu
+    public enum State
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    // Menyimpan state saat ini
+    private State stateSekarang;
+
+    // Constructor: state awal terkunci
+    public DoorMachine()
+    {
+        stateSekarang = State.Terkunci;
+        TampilkanState();
+    }
+
+    // Method untuk mengunci pintu
+    public void KunciPintu()
+    {
+        if (stateSekarang == State.Terkunci)
+        {
+            Console.WriteLine("Pintu sudah dalam keadaan terkunci.");
+        }
+        else
+        {
+            stateSekarang = State.Terkunci;
+            TampilkanState();
+        }
+    }
+
+    // Method untuk membuka pintu
+    public void BukaPintu()
+    {
+        if (stateSekarang == State.Terbuka)
+        {
+            Console.WriteLine("Pintu sudah dalam keadaan terbuka.");
+        }
+        else
+        {
+            stateSekarang = State.Terbuka;
+            TampilkanState();
+        }
+    }
+
+    // Method untuk menampilkan state saat ini
+    private void TampilkanState()
+    {
+        if (stateSekarang == State.Terkunci)
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+        else if (stateSekarang == State.Terbuka)
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
---
+++ 04_Automata_dan_Table-Driven_Constructionn/tpmod4_2311104042/tpmod4_2311104042/Program1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        DoorMachine pintu = new DoorMachine();
+
+        pintu.BukaPintu();  
+        pintu.KunciPintu(); 
+        pintu.KunciPintu(); 
+        pintu.BukaPintu();  
+        pintu.BukaPintu();  
+    }
+}

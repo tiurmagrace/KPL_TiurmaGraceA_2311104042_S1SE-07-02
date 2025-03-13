19ee89cde05b9ac8871f91536e864fe1ed819178
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-14 04:58:57 +0700

    menambahkan class KodeBuah

---
+++ 04_Automata_dan_Table-Driven_Constructionn/tjmod4_2311104042/tjmod4_2311104042/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama buah: ");
+        string inputBuah = Console.ReadLine();
+        Console.WriteLine("Kode untuk {0} adalah: {1}", inputBuah, KodeBuah.getKodeBuah(inputBuah));
+    }
+}

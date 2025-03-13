57586d2ad5563468a8a7b3c233dd5dfa1caeb79f
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-14 03:59:23 +0700

    menambahkan implementasi dengan table-driven

---
+++ 04_Automata_dan_Table-Driven_Constructionn/tpmod4_2311104042/tpmod4_2311104042/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using tpmod4_2311104042;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama kelurahan: ");
+        string? kelurahan = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(kelurahan))
+        {
+            Console.WriteLine("Nama kelurahan tidak boleh kosong.");
+        }
+        else
+        {
+            string kodePos = KodePos.GetKodePos(kelurahan);
+            Console.WriteLine($"Kode pos {kelurahan}: {kodePos}");
+        }
+
+        Console.ReadLine();
+    }
+}

7541741d7f429978fb06975745a9e0946faba7b5
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-14 05:34:31 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/tpmod5_2311104042/tpmod5_2311104042/HaloGeneric.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// Class dengan Generic Method
+class HaloGeneric
+{
+    // Method generic untuk menyapa user
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}

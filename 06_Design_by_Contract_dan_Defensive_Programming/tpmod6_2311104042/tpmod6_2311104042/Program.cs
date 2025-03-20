961993013f73fd8186807f3eb2b8b809fa7fb568
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-20 21:00:25 +0700

    menambahkan class SayaTubeVideo.cs

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/tpmod6_2311104042/tpmod6_2311104042/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Tiurma Grace Angelina");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
+

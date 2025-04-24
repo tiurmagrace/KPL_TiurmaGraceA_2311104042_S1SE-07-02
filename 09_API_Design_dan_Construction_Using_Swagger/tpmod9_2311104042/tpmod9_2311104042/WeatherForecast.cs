18e8492458aeb9f9b1f9f7d323bcc4a7a15a3eea
tiurmagrace <tiursihaloho210905@gmail.com>
2025-04-24 21:38:28 +0700

    up 1 .gitignore

---
+++ 09_API_Design_dan_Construction_Using_Swagger/tpmod9_2311104042/tpmod9_2311104042/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace tpmod9_2311104042
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}

aa66a0dc69f147f70cc07c77fd0159520739d39b
tiurmagrace <tiursihaloho210905@gmail.com>
2025-04-24 20:34:40 +0700

    up guided, salah penamaan kk maap

---
+++ 08_Runtime_Configuration_dan_Internationalization/tjmod8_2311104042/tjmod8_2311104042/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tjmod8_2311104042.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

43ace4e3f0543761231022036c219a7ca78f1d7e
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-05 23:27:59 +0700

    up menambahkan library AljabarLibraries

---
+++ 10_Library_Construction/tpmod10_2311104042/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = b * b - 4 * a * c;
+            if (diskriminan < 0) return new double[0]; 
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilA = a * a;
+            double hasilB = 2 * a * b;
+            double hasilC = b * b;
+
+            return new double[] { hasilA, hasilB, hasilC };
+        }
+    }
+}
---
+++ 10_Library_Construction/tpmod10_2311104042/ConsoleAppAljabar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace ConsoleAppAljabar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mencari Akar Persamaan Kuadrat dari x^2 - 3x - 10");
+            double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine($"Akar: {akar[0]} dan {akar[1]}");
+
+            Console.WriteLine("\nMenghitung Hasil Kuadrat dari 2x - 3");
+            double[] hasil = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine($"Hasil Kuadrat: {hasil[0]}x^2 + ({hasil[1]})x + {hasil[2]}");
+        }
+    }
+}

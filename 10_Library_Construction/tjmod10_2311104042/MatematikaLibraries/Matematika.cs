d6d1d83aed9ada0bf9dc5ee1c10fc48c4b2d4764
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-05 23:07:27 +0700

    up MatematikaLibraries

---
+++ 10_Library_Construction/tjmod10_2311104042/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MatematikaLibraries
+
+{
+    public static class Matematika
+    {
+        public static int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a);
+        }
+
+        public static int KPK(int a, int b)
+        {
+            return Math.Abs(a * b) / FPB(a, b);
+        }
+
+        public static string Turunan(int[] koef)
+        {
+            string hasil = "";
+            for (int i = 1; i < koef.Length; i++)
+            {
+                int k = koef[i] * i;
+                string term = $"{(k >= 0 && hasil != "" ? "+" : "")}{k}x^{i - 1}";
+                hasil += term;
+            }
+            return hasil;
+        }
+
+        public static string Integral(int[] koef)
+        {
+            string hasil = "";
+            for (int i = 0; i < koef.Length; i++)
+            {
+                double k = (double)koef[i] / (i + 1);
+                string term = $"{(k >= 0 && hasil != "" ? "+" : "")}{k}x^{i + 1}";
+                hasil += term;
+            }
+            return hasil + "+C";
+        }
+    }
+}
---
+++ 10_Library_Construction/tjmod10_2311104042/ShowCase/Showcase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Showcase
+{
+    static void Main()
+    {
+        int a = 12, b = 18;
+        Console.WriteLine($"FPB dari {a} dan {b} = {Matematika.FPB(a, b)}");
+        Console.WriteLine($"KPK dari {a} dan {b} = {Matematika.KPK(a, b)}");
+
+        int[] polinomial = { -3, 6, 0, -4 }; 
+        Console.WriteLine("Turunan dari -3 + 6x - 4x^3 = " + Matematika.Turunan(polinomial));
+        Console.WriteLine("Integral dari -3 + 6x - 4x^3 = " + Matematika.Integral(polinomial));
+    }
+}

bb918bdbe5ae6b22f98633861ee71c6bf0a354a8
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-08 23:07:58 +0700

    up tpmod12 gui with unit testing

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/tpmod12_2311104042/tpmod12_2311104042/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmod12_2311104042
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input;
+            if (int.TryParse(textBox1.Text, out input))
+            {
+                label1.Text = TandaBilanganHelper.CariTandaBilangan(input);
+            }
+            else
+            {
+                label1.Text = "Input tidak valid!";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/tpmod12_2311104042/tpmod12_2311104042/TandaBilanganHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod12_2311104042
+{
+    public static class TandaBilanganHelper
+    {
+        public static string CariTandaBilangan(int a)
+        {
+            if (a < 0) return "Negatif";
+            if (a > 0) return "Positif";
+            return "Nol";
+        }
+    }
+}

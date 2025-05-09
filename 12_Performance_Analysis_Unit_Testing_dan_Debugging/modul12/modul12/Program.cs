a9da47d7336c9abe2061e7593c8be511cbcac38d
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-09 10:05:53 +0700

    up guided

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/modul12/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace modul12
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string result = DetermineGrade(89);
+            Console.WriteLine(result);
+
+        }
+        public static String DetermineGrade(int score)
+        {
+            if (score > 100 || score < 0) return null;
+            Dictionary<int, String> GradeMap = new Dictionary<int, string>()
+            {
+                {90, "A"},
+                {80, "B"},
+                {70, "C"},
+                {60, "D"},
+                {0, "E"},
+            };
+            foreach (var kv in GradeMap.OrderByDescending(kv => kv.Key))
+            {
+                if (score >= kv.Key)
+                {
+                    return kv.Value;
+                }
+            }
+            return null;
+        }
+    }
+}
+

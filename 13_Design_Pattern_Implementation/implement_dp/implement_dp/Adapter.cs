a0138f8000698209b07cee649b7953ea172519ce
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-16 10:25:49 +0700

    up guided

---
+++ 13_Design_Pattern_Implementation/implement_dp/implement_dp/Adapter.cs
@@ -0,0 +1,30 @@
+﻿namespace adapter_dp
+{
+    public interface IAmericanPlug
+    {
+        void PlugIn();
+    }
+
+    public class IndonesianPlug
+    {
+        public void Sambungkan()
+        {
+            Console.WriteLine("Colokan Indonesia tersambung");
+        }
+    }
+
+    public class PlugAdaptor : IAmericanPlug
+    {
+        private readonly IndonesianPlug _plug;
+        public PlugAdaptor(IndonesianPlug indonesianPlug)
+        {
+            _plug = indonesianPlug;
+        }
+
+        public void PlugIn()
+        {
+            Console.WriteLine("Menggunakan adapter colokan indonesia ke amerika");
+            _plug.Sambungkan();
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/implement_dp/implement_dp/Command.cs
@@ -0,0 +1,70 @@
+﻿namespace command_dp
+{
+    public interface ICommand
+    {
+        public void Execute();
+    }
+
+    public class Television
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Teelevision is on");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine("Teelevision is off");
+        }
+    }
+
+    public class TelevisionTurnOn : ICommand
+    {
+        private readonly Television? _television;
+
+        public TelevisionTurnOn(Television? television)
+        {
+            _television = television;
+        }
+        public void Execute()
+        {
+            _television!.TurnOn();
+        }
+    }
+    public class TelevisionTurnOff : ICommand
+    {
+        private readonly Television? _television;
+
+        public TelevisionTurnOff(Television? television)
+        {
+            _television = television;
+        }
+        public void Execute()
+        {
+            _television!.TurnOff();
+        }
+    }
+
+    public class RemoteTv
+    {
+        private ICommand? turnOn;
+        private ICommand? turnOff;
+
+        public void setTurnOn(ICommand command)
+        {
+            turnOn = command;
+        }
+        public void setTurnOff(ICommand command)
+        {
+            turnOff = command;
+        }
+        public void TurnOnTV()
+        {
+            turnOn?.Execute();
+        }
+
+        public void TurnOffTV()
+        {
+            turnOff?.Execute();
+        }
+    }
+}

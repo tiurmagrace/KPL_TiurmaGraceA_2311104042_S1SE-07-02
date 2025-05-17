814f6e5888a55b19ad3a521952083b3bd5d4d109
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-17 23:38:15 +0700

    IMPLEMENTASI DAN PEMAHAMAN DESIGN PATTERN SINGLETON

---
+++ 13_Design_Pattern_Implementation/tjmod13_2311104042/tjmod13_2311104042/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tj13_2311104042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data1 = PusatDataSingleton.GetDataSingleton();
+            var data2 = PusatDataSingleton.GetDataSingleton();
+
+            data1.AddSebuahData("Tiurma Grace Angelina");
+            data1.AddSebuahData("caca");
+            data1.AddSebuahData("cici");
+            data1.AddSebuahData("Asisten: cece");
+
+            Console.WriteLine("Data2 sebelum penghapusan:");
+            data2.PrintSemuaData();
+
+            data2.HapusSebuahData(3);
+
+            Console.WriteLine("\nData1 setelah penghapusan dari data2:");
+            data1.PrintSemuaData();
+
+            Console.WriteLine($"\nJumlah data (data1): {data1.GetSemuaData().Count}");
+            Console.WriteLine($"Jumlah data (data2): {data2.GetSemuaData().Count}");
+
+            Console.WriteLine("\nTekan tombol apa saja untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/tjmod13_2311104042/tjmod13_2311104042/PusatDataSingleton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tj13_2311104042
+{
+    public class PusatDataSingleton
+    {
+        private static PusatDataSingleton _instance;
+
+        public List<string> DataTersimpan { get; set; }
+
+        private PusatDataSingleton()
+        {
+            DataTersimpan = new List<string>();
+        }
+
+        public static PusatDataSingleton GetDataSingleton()
+        {
+            if (_instance == null)
+            {
+                _instance = new PusatDataSingleton();
+            }
+            return _instance;
+        }
+
+        public List<string> GetSemuaData()
+        {
+            return DataTersimpan;
+        }
+
+        public void PrintSemuaData()
+        {
+            Console.WriteLine("Isi Data:");
+            foreach (var data in DataTersimpan)
+            {
+                Console.WriteLine("- " + data);
+            }
+        }
+
+        public void AddSebuahData(string input)
+        {
+            DataTersimpan.Add(input);
+        }
+
+        public void HapusSebuahData(int index)
+        {
+            if (index >= 0 && index < DataTersimpan.Count)
+            {
+                DataTersimpan.RemoveAt(index);
+            }
+        }
+    }
+}
